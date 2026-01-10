4aef3616848902b5896cd70905221137f495653c
Lilythecreator <klilia310@gmail.com>
2026-01-10 17:42:58 +0300

    git push

--- src/main/java/org/example/TrArea.java
+++ src/main/java/org/example/TrArea.java
@@ -4,7 +4,7 @@ public class TrArea {
 
     public static double getArea(int height, int base) {
         if (height <= 0 || base <= 0 ) {
-            throw new IllegalArgumentException("Длины сторон должны быть больше 0");
+            throw new IllegalArgumentException("Длины высоты и основания должны быть больше 0");
         }
         return 0.5 * height * base;
     }
